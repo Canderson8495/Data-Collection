a587739280b41c3144ce135f6f45e750b238f9ce
I'm a meme <Thrillechs@gmail.com>
2018-02-12 17:06:41 -0500

    Parsing: Implemented
    In this commit, Parsing was implemented the the FileReadWrite class.  I also created a quick Person class to store the parsed data values within groups.

--- src/pkg/DataCollection.java
+++ src/pkg/DataCollection.java
@@ -5,40 +5,10 @@ import java.io.File;
 public class DataCollection {
 
 	public static void main(String[] args) {
-		System.out.println("Testing my own branch");
-		Scanner scan = new Scanner(System.in);
-		int year = scan.nextInt();
-		String name = scan.next();
-		char gender = scan.next().charAt(0);
-		java.io.File file = new java.io.File("babynameranking"+year+".txt");
-		int rank;
-		String maleName;
-		int numOfMaleName;
-		String femaleName;
-		int numOfFemaleName;
-		try {
-			Scanner fileReader = new Scanner(file);
-			for(int x = 0; x < 1000; x++) {
-				rank = fileReader.nextInt();
-				maleName = fileReader.next();
-				numOfMaleName = fileReader.nextInt();
-				femaleName = fileReader.next();
-				numOfFemaleName = fileReader.nextInt();
-				if(gender == 'M') {
-					if(name == maleName) {
-						System.out.println("The rank is " + rank);
-					}
-				}else {
-					if(name == femaleName) {
-						System.out.println("The rank is " + rank);
-					}
-				}
-			}
-		}catch(FileNotFoundException e){
-			
+		java.io.File file = new java.io.File("C:\\Code\\Java\\Txt.txt");
+		ArrayList<Person> customers = FileReadWrite.parse(file);
+		for(Person p : customers) {
+			System.out.println(p);
 		}
-		
-		
-	}
-
+	} 
 }
---
+++ src/pkg/Person.java
@@ -0,0 +1,76 @@
+package pkg;
+
+public class Person {
+	String firstName, lastName, address, roomNum, city, state, zipCode;
+	double amountOwed;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(String roomNum) {
+		this.roomNum = roomNum;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public double getAmountOwed() {
+		return amountOwed;
+	}
+	public void setAmountOwed(double amountOwed) {
+		this.amountOwed = amountOwed;
+	}
+	public Person(String firstName, String lastName, String address, String roomNum, String city, String state,
+			String zipCode, double amountOwed) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.roomNum = roomNum;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.amountOwed = amountOwed;
+	}
+	public Person() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", roomNum="
+				+ roomNum + ", city=" + city + ", state=" + state + ", zipCode=" + zipCode + ", amountOwed="
+				+ amountOwed + "]";
+	}
+
+}
