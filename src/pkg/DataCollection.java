448716e92dee6f10ed1bd78d07d50a00bba643b2
jnguyenv31 <36414868+jnguyenv31@users.noreply.github.com>
2018-02-13 22:50:54 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "1st Attempt?""""""
    This reverts commit 30c28bf7b491c2cd133056e7ec6a8535a8a1f0b1.

--- src/pkg/DataCollection.java
+++ src/pkg/DataCollection.java
@@ -2,14 +2,31 @@ package pkg;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.io.File;
+
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DataCollection {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		java.io.File file = new java.io.File("C:\\Code\\Java\\Txt.txt");
 		ArrayList<Person> customers = FileReadWrite.parse(file);
 		for(Person p : customers) {
 			p.accrueMonthlyInterest();
 			System.out.println(p);
+			
+			String fileName = ("C:\\Code\\Java\\Txt.txt");
+			FileReader reader = new FileReader("C:\\Code\\Java\\Txt.txt");
+			BufferedReader buffer = new BufferedReader(reader);
+			PrintWriter writer = new PrintWriter ("C:\\Code\\Java\\Txt.txt");
+			
+			while ((fileName = buffer.readLine()) !=null) {
+			writer.println ("\"C:\\\\Code\\\\Java\\\\Txt.txt\"");
 		}
 	} 
+}
+
+
 }
